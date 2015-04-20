c65e15318927eef13ff62a820aafb1ece207d2db
vanbrayne <lars.lindberg@xlent.se>
2015-04-20 20:51:01 +0200

    Added more configurable messages.

--- src/main/java/net/eithon/plugin/freebuild/Config.java
+++ src/main/java/net/eithon/plugin/freebuild/Config.java
@@ -34,6 +34,10 @@ public class Config {
 	public static class M {
 		public static ConfigurableMessage waitForCoolDown;
 		public static ConfigurableMessage mustBeInFreebuildWord;
+		public static ConfigurableMessage alreadyOn;
+		public static ConfigurableMessage alreadyOff;
+		public static ConfigurableMessage activated;
+		public static ConfigurableMessage deactivated;
 
 		static void load(Configuration config) {
 			waitForCoolDown = config.getConfigurableMessage(
@@ -42,6 +46,18 @@ public class Config {
 			mustBeInFreebuildWord = config.getConfigurableMessage(
 					"messages.MustBeInFreebuildWord", 0, 
 					"You can only switch between survival and freebuild in the SurvivalFreebuild world.");
+			alreadyOn = config.getConfigurableMessage(
+					"messages.AlreadyOn_0", 0, 
+					"Freebuild mode is already active.");
+			alreadyOff = config.getConfigurableMessage(
+					"messages.AlreadyOff_0", 0, 
+					"Survival mode is already active (freebuild is OFF).");
+			activated = config.getConfigurableMessage(
+					"messages.Activated_0", 0, 
+					"Freebuild mode is now active.");
+			deactivated = config.getConfigurableMessage(
+					"messages.Deactivated_0", 0, 
+					"Survival mode is now active (freebuild is OFF).");
 		}		
 	}
 
