cfad573f7afc0175f4f8f5fa7d049cece8a316a9
Lars Lindberg <lars.lindberg@me.com>
2015-06-14 21:18:34 +0200

    No slash

--- src/main/java/net/eithon/plugin/freebuild/Config.java
+++ src/main/java/net/eithon/plugin/freebuild/Config.java
@@ -33,7 +33,7 @@ public class Config {
 
 		static void load(Configuration config) {
 			setSpeed = config.getConfigurableCommand("commands.SetSpeed", 2,
-					"/speed fly %.2f %s");
+					"speed fly %.2f %s");
 		}
 
 	}
